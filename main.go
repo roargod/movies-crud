9f1a33ef014cd2b4e2b7f13534f2dcb883b27e45
沉着冷静 <2478261806@qq.com>
2024-10-25 16:19:09 +0800

    实现查找所有电影和查找目标id电影

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 // Movie结构体
@@ -22,8 +23,25 @@ type Director struct {
 var movies []Movie
 
 func getMovies(c *gin.Context) {
+	c.JSON(http.StatusOK, movies)
 }
 func getMovie(c *gin.Context) {
+	// 切出其中的id字段
+	id, ok := c.Params.Get("id")
+	if !ok {
+		c.JSON(http.StatusOK, gin.H{"err": "无效的id"})
+		return
+	}
+	// 遍历寻找该电影id
+	for i := 0; i < len(movies); i++ {
+		// 找到就返回该电影
+		if movies[i].ID == id {
+			c.JSON(http.StatusOK, movies[i])
+			return
+		}
+	}
+	// 走到这，没找到
+	c.JSON(http.StatusOK, "没找到对应电影")
 }
 func createMovie(c *gin.Context) {
 
