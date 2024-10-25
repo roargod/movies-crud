a36ad5cf73ab9176da62791b495719f1a18a86a4
沉着冷静 <2478261806@qq.com>
2024-10-25 16:34:23 +0800

    实现删除电影和创建电影功能

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"math/rand"
 	"net/http"
+	"strconv"
 )
 
 // Movie结构体
@@ -44,13 +46,31 @@ func getMovie(c *gin.Context) {
 	c.JSON(http.StatusOK, "没找到对应电影")
 }
 func createMovie(c *gin.Context) {
-
+	var movie Movie
+	movie.ID = strconv.Itoa(rand.Intn(10000))
+	movies = append(movies, movie)
+	c.JSON(http.StatusOK, movie)
 }
 func updateMovie(c *gin.Context) {
 
 }
 func deleteMovie(c *gin.Context) {
-
+	// 切出其中的id字段
+	id, ok := c.Params.Get("id")
+	if !ok {
+		c.JSON(http.StatusOK, gin.H{"err": "无效的id"})
+		return
+	}
+	// 遍历寻找该电影id
+	for i := 0; i < len(movies); i++ {
+		// 找到就删除该电影
+		if movies[i].ID == id {
+			//c.JSON(http.StatusOK, movies[i])
+			movies = append(movies[:i], movies[i+1:]...)
+			break
+		}
+	}
+	c.JSON(http.StatusOK, movies)
 }
 func main() {
 	// 创建路由
